2a003b9745b2b2b340a293aeed76b6703fa825bb
Sherry McCarty <sherrydmccarty@gmail.com>
2020-08-04 13:51:30 -0400

    seed data fixed

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,12 +1,13 @@
 BEGIN;
 
 TRUNCATE
-  "company",
-  "users",
-  "client",
-  "report";
+  company,
+  users,
+  client,
+  report
+  RESTART IDENTITY CASCADE;
 
-INSERT INTO "company" ("id", "name", "location")
+INSERT INTO company (id, name, location)
 VALUES
   (
     1,
@@ -14,7 +15,7 @@ VALUES
     '1234 reenergize ave, fresco ca, 90345'
   );
 
-INSERT INTO "users" ("id", "name", "user_name", "password", "company_id", "admin", "boss_id")
+INSERT INTO users (id, name, user_name, password, company_id, admin, boss_id)
 VALUES
   (
     1,
@@ -23,10 +24,11 @@ VALUES
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG',
     1,
-    true
+    true,
+    null
   );
 
-INSERT INTO "client" ("id", "name", "location", "sales_rep_id", "company_id", "hours_of_operation", "currently_closed", "general_manager", "notes")
+INSERT INTO client (id, name, location, sales_rep_id, company_id, hours_of_operation, currently_closed, general_manager, notes)
 VALUES
   (
     1,
@@ -34,21 +36,20 @@ VALUES
     '4630 Troost Ave, Kansas City MO, 64110',
     1,
     1,
-    '
-      { “close”: { “day”: 0, “time”: “2230” }, “open”: { “day”: 0, “time”: “1200” } }, { “close”: { “day”: 1, “time”: “2300” }, “open”: { “day”: 1, “time”: “1130” } },   { “close”: { “day”: 2, “time”: “2230” }, “open”: { “day”: 2, “time”: “1200” } }, { “close”: { “day”: 3, “time”: “2300” }, “open”: { “day”: 3, “time”: “1130” } },   { “close”: { “day”: 4, “time”: “2230” }, “open”: { “day”: 4, “time”: “1200” } }, { “close”: { “day”: 5, “time”: “2300” }, “open”: { “day”: 5, “time”: “1130” } },   { “close”: { “day”: 6, “time”: “2230” }, “open”: { “day”: 6, “time”: “1200” } }
-    ',
+    ' “close” => "{ “day”: 0, “time”: “2230” }", “open” => "{ “day”: 0, “time”: “1200” }" , “close” => "{ “day”: 1, “time”: “2300” }", “open” => "{ “day”: 1, “time”: “1130” }", “close” => "{ “day”: 2, “time”: “2230” }", “open” => "{ “day”: 2, “time”: “1200” }", “close” => "{ “day”: 3, “time”: “2300” }", “open” => "{ “day”: 3, “time”: “1130” }", “close” => "{ “day”: 4, “time”: “2230” }", “open” => "{ “day”: 4, “time”: “1200” }", “close” => "{ “day”: 5, “time”: “2300” }", “open” => "{ “day”: 5, “time”: “1130” }", “close” => "{ “day”: 6, “time”: “2230” }", “open” => "{ “day”: 6, “time”: “1200” }"',
     false,
     'sam wise',
     'good displays'
   );
 
-INSERT INTO "report" ("id", "client_id", "sales_rep_id", "date", "notes", "photo_url")
+INSERT INTO report (id, client_id, sales_rep_id, date, notes, photo_url)
 VALUES (
   1,
   1,
   1,
-  new Date(),
-  'good times'
+  '2016-06-23T02:10:25.000Z',
+  'good times',
+  null
 );
 
 
