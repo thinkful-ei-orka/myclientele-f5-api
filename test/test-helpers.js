5e87cf344c21cc2de6a762ae50487e3acdf45b33
Sherry McCarty <sherrydmccarty@gmail.com>
2020-08-04 12:31:36 -0400

    updated migration file name

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,3 +1,19 @@
 const knex = require('knex')
 const bcrypt = require('bcrypt.js')
 const jwt = require('jsonwebtoken')
+
+function makeKnexInstance() {
+  return knex({
+    client: 'pg',
+    connection: process.env.TEST_DATABASE_URL
+  })
+}
+
+function makeUsersArray() {
+  return [
+    {
+      id: 1,
+      
+    }
+  ]
+}
