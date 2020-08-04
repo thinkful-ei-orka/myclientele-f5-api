0bc50dcbb4fcc198bd6de87af05f1468666b9b70
Sherry McCarty <sherrydmccarty@gmail.com>
2020-08-04 12:35:22 -0400

    test-helpers started

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -9,11 +9,28 @@ function makeKnexInstance() {
   })
 }
 
-function makeUsersArray() {
+function makeTestUsers() {
   return [
     {
       id: 1,
-      
+      name: 'test-user-1',
+      user_name: 'test-user-name-1',
+      password: 'password',
+      company_id: 1,
+      admin: true
+    },
+    {
+      id: 2,
+      name: 'test-user-2',
+      user_name: 'test-user-name-2',
+      password: 'password',
+      company_id: 2,
+      admin: true
     }
   ]
 }
+
+function makeAuthHeader() {
+
+}
+
