7ca0763794404791f0e9b9f956fcf75cf83774bd
Taeil Kwak <taeil2@gmail.com>
2020-08-04 15:19:21 -0500

    updates to auth router

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -4,14 +4,13 @@ const authRouter = express.Router();
 const jsonBodyParser = express.json();
 const { requireAuth } = require('../middleware/jwt-auth');
 
-authRouter.post('/refresh', requireAuth, (req, res) => {
+authRouter.put('/refresh', requireAuth, (req, res) => {
     const sub = req.user.user_name;
     const payload = { user_id: req.user.id };
     res.send({
         authToken: AuthService.createJwt(sub, payload),
     });
 });
-
 
 authRouter.post('/login', jsonBodyParser, (req, res, next) => {
     const { user_name, password } = req.body;
